1a56372eefb78e08c182caa546d6698abc861122
Roberto Cestari <robertotcestari@gmail.com>
2021-03-30 15:27:22 -0300

    changes image

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -51,9 +51,10 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 }
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
-    const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+    const defaultImage =
+      theme === "light"
+        ? "https://informativos.trilhante.com.br/img/informativos/logo.svg"
+        : "https://informativos.trilhante.com.br/img/informativos/logo.svg";
 
     if (!images || !images[0]) {
         return [defaultImage];
